b5214f65835937bf00a85ce686719f624422947d
Gregor Budweiser <gregor.budweiser@gmail.com>
2017-06-28 23:09:29 +0200

    aligned memory acces in inner loop of adler checksum.

--- source/pngenc/adler32.c
+++ source/pngenc/adler32.c
@@ -1,4 +1,5 @@
 #include "adler32.h"
+#include <stdlib.h>
 
 uint32_t adler_get_checksum(const pngenc_adler32 * adler) {
     return (adler->s2 << 16) | adler->s1;
@@ -10,12 +11,22 @@ void adler_init(pngenc_adler32 * adler) {
 }
 
 void adler_update(pngenc_adler32 * adler, const uint8_t * data,
-                  const uint32_t length) {
-    const register uint32_t length8 = length >> 3;
-    const uint64_t *data64 = (const uint64_t*)data;
+                  uint32_t length) {
 
     register uint32_t s1 = adler->s1;
     register uint32_t s2 = adler->s2;
+
+    // align to 8 byte
+    while(length && (((size_t)data & 0x7) != 0)) {
+        s1 = (s1 + data[0]) % 65521;
+        s2 = (s2 + s1) % 65521;
+        data++;
+        length--;
+    }
+
+    const register uint32_t length8 = length >> 3;
+    const uint64_t *data64 = (const uint64_t*)data;
+
 
     uint32_t n = 0;
     uint32_t k = 0;
--- source/pngenc/adler32.h
+++ source/pngenc/adler32.h
@@ -8,5 +8,5 @@ typedef struct _pngenc_adler32 {
 
 void adler_init(pngenc_adler32 * adler);
 void adler_update(pngenc_adler32 * adler, const uint8_t * data,
-                  const uint32_t length);
+                  uint32_t length);
 uint32_t adler_get_checksum(const pngenc_adler32 * adler);
