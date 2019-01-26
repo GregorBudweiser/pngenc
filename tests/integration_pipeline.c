2962469fef36c008a2cc587b8de892d4f5f1746a
Gregor Budweiser <gregor.budweiser@gmail.com>
2019-01-26 12:52:59 +0100

    Updated performance tests.

--- tests/integration_pipeline.c
+++ tests/integration_pipeline.c
@@ -8,8 +8,7 @@ int integration_pipeline(int argc, char* argv[]) {
     const int H = 1080;
     const int C = 3;
 
-    uint8_t * buf = (uint8_t*)malloc(W*H*C);
-
+    uint8_t * buf;
     {
         // Load raw image data (1920x1080x3 bytes)
         FILE * file = fopen("data.bin", "rb");
@@ -19,25 +18,35 @@ int integration_pipeline(int argc, char* argv[]) {
             printf("Expecting 1920x1080x3 bytes of data.\n");
             return 0;
         }
+        buf = (uint8_t*)malloc(W*H*C);
         fread(buf, C, W*H, file);
         fclose(file);
     }
+    printf("Could not open output file!\n");
 
     pngenc_image_desc desc;
     desc.data = buf;
     desc.width = W;
     desc.height = H;
     desc.num_channels = C;
-    desc.row_stride = W*C;
+    desc.row_stride = W * C;
     desc.bit_depth = 8;
     desc.strategy = PNGENC_HUFFMAN_ONLY_WITH_PNG_ROW_FILTER1;
 
     pngenc_pipeline pipeline;
     pipeline = pngenc_pipeline_create(&desc, &write_to_file_callback, NULL);
 
-    FILE * file = fopen("/dev/null", "wb");
-    if(file == NULL)
+#if defined(WIN32) || defined(__WIN32)
+    const char * devNull = "nul";
+#else
+    const char * devNull = "/dev/null";
+#endif
+
+    FILE * file = fopen(devNull, "wb");
+    if (file == NULL) {
+        printf("Could not open output file!\n");
         return PNGENC_ERROR_FILE_IO;
+    }
 
     for(int i = 0; i < 20; i++) {
         TIMING_START;
@@ -49,7 +58,7 @@ int integration_pipeline(int argc, char* argv[]) {
 
     for(int i = 0; i < 20; i++) {
         TIMING_START;
-        pngenc_write_file(&desc, "/dev/null");
+        pngenc_write_file(&desc, devNull);
         TIMING_END;
     }
 
@@ -57,6 +66,7 @@ int integration_pipeline(int argc, char* argv[]) {
         return PNGENC_ERROR_FILE_IO;
 
     pngenc_pipeline_destroy(pipeline);
+    free(buf);
 
     return PNGENC_SUCCESS;
 }
--- tests/perf_huffman.c
+++ tests/perf_huffman.c
@@ -7,7 +7,7 @@ static const int W = 1920;
 static const int H = 1080;
 static const int C = 3;
 
-void perf_add(const uint8_t * buf) {
+void perf_add(const uint8_t * src, uint8_t * dst) {
     huffman_encoder encoder;
     huffman_encoder_init(&encoder);
 
@@ -17,7 +17,7 @@ void perf_add(const uint8_t * buf) {
         TIMING_START;
         const int end = C*W*H/8;
         for(j = 0; j < end; j++) {
-            volatile register uint64_t tmp = ((uint64_t*)buf)[j];
+            volatile register uint64_t tmp = ((uint64_t*)src)[j];
             UNUSED(tmp);
         }
         TIMING_END;
@@ -26,23 +26,31 @@ void perf_add(const uint8_t * buf) {
     printf("optimized\n");
     for(i = 0; i < 5; i++) {
         TIMING_START;
-        huffman_encoder_add(encoder.histogram, buf, C*W*H);
+        huffman_encoder_add(encoder.histogram, src, C*W*H);
         TIMING_END;
     }
 
     printf("simple\n");
     for(i = 0; i < 5; i++) {
         TIMING_START;
-        huffman_encoder_add_simple(encoder.histogram, buf, C*W*H);
+        huffman_encoder_add_simple(encoder.histogram, src, C*W*H);
+        TIMING_END;
+    }
+
+    printf("memset\n");
+    for(i = 0; i < 5; i++) {
+        TIMING_START;
+        memset(dst, 0, C*W*H);
         TIMING_END;
     }
 }
 
-void perf_encode(const uint8_t * buf, uint8_t * dst) {
+int perf_encode(const uint8_t * buf, uint8_t * dst) {
     huffman_encoder encoder;
     huffman_encoder_init(&encoder);
     huffman_encoder_add(encoder.histogram, buf, C*W*H);
-    huffman_encoder_build_tree_limited(&encoder, 15, 0.95);
+    RETURN_ON_ERROR(huffman_encoder_build_tree_limited(&encoder, 15, 0.8));
+    RETURN_ON_ERROR(huffman_encoder_build_codes_from_lengths(&encoder));
 
     uint64_t offset;
     int i;
@@ -91,28 +99,34 @@ void perf_encode(const uint8_t * buf, uint8_t * dst) {
         huffman_encoder_encode64_3(&encoder, buf, C*W*H, dst, &offset);
         TIMING_END;
     }
+
+    return 0;
 }
 
 int perf_huffman(int argc, char* argv[]) {
     UNUSED(argc);
     UNUSED(argv);
-    uint8_t * buf = (uint8_t*)malloc(W*H*C);
+
+    uint8_t * src = (uint8_t*)malloc(W*H*C);
+    uint8_t * dst = (uint8_t*)malloc(W*H*C);
     {
         FILE * f = fopen("data.bin", "rb");
         if(f == 0) {
             printf("Could not open file: data.bin\n");
+            free(src);
+            free(dst);
             return -1;
         }
-        fread(buf, C, W*H, f);
+        fread(src, C, W*H, f);
         fclose(f);
     }
 
-    uint8_t * dst = (uint8_t*)malloc(W*H*C);
 
-    perf_add(buf);
-    perf_encode(buf, dst);
+    perf_add(src, dst);
+    RETURN_ON_ERROR(perf_encode(src, dst));
 
-    free(buf);
+    free(src);
+    free(dst);
     return 0;
 }
 
